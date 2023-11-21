7ba9ed67781290ae8808b623d6755ae2c7ab085b
Kulwinder <laddiaulakh2002@gmail.com>
2023-11-21 09:39:00 +0530

    created flask api

---
+++ flask-api/ML_CODE.py
@@ -0,0 +1,118 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import cv2
+from tensorflow import keras
+import tensorflow as tf 
+from tensorflow import keras
+from keras.applications import ResNet50
+from pathlib import Path
+from tqdm import tqdm
+import PIL
+import matplotlib.pyplot as plt
+import os
+print("Imported Successfully")
+
+# import each path of the image classes
+glaucoma = Path(r'./Dataset/glaucoma')
+cataract = Path(r'./Dataset/cataract')
+normal = Path(r'./Dataset/normal')
+diabetic_retinopathy = Path(r'./Dataset/diabetic_retinopathy')
+
+# create a dataframe with the file path and the labels
+disease_type = [glaucoma, cataract,normal,diabetic_retinopathy]
+df = pd.DataFrame()
+from tqdm import tqdm
+for types in disease_type:
+    for imagepath in tqdm(list(types.iterdir()), desc= str(types)):
+        df = pd.concat([df, pd.DataFrame({'image': [str(imagepath)],'disease_type': [disease_type.index(types)]})], ignore_index=True)
+
+df
+
+df['disease_type'] = df['disease_type'].map({0:'glaucoma',1:'cataract',2:'normal',3:'diabetic_retinopathy'})
+# check the lebel count to verify it has been mapped
+df.disease_type.value_counts()
+# randomising the dataset
+df1 = df.sample(frac=1).reset_index(drop=True)
+
+#import necessary libraries for the model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.applications.resnet50 import preprocess_input,decode_predictions
+from keras import layers
+from sklearn.metrics import classification_report, confusion_matrix
+
+# augumentation of images
+datagen = ImageDataGenerator(preprocessing_function=preprocess_input,validation_split=0.2)
+# create the train data
+train_data = datagen.flow_from_dataframe(dataframe=df1,
+                                          x_col ='image',
+                                          y_col = 'disease_type',
+                                          target_size=(224,224),
+                                          class_mode = 'categorical',
+                                          batch_size = 32,
+                                          shuffle = True,
+                                          subset = 'training')
+
+# create the validation data
+valid_data = datagen.flow_from_dataframe(dataframe=df1,
+                                          x_col ='image',
+                                          y_col = 'disease_type',
+                                          target_size=(224,224),
+                                          class_mode = 'categorical',
+                                          batch_size = 32,
+                                          shuffle = False,
+                                          subset = 'validation')
+
+labels=[key for key in train_data.class_indices]
+num_classes = len(disease_type)
+
+model = keras.Sequential([ 
+    layers.Rescaling(1./255, input_shape=(224,224, 3)), 
+    layers.Conv2D(16, 3, padding='same', activation='relu'), 
+    layers.MaxPooling2D(), 
+    layers.Conv2D(32, 3, padding='same', activation='relu'), 
+    layers.MaxPooling2D(), 
+    layers.Conv2D(64, 3, padding='same', activation='relu'), 
+    layers.MaxPooling2D(), 
+    layers.Flatten(), 
+    layers.Dense(128, activation='relu'), 
+    layers.Dense(num_classes,activation='softmax') 
+]) 
+model.compile(optimizer='adam', 
+              loss=tf.keras.losses.categorical_crossentropy, 
+              metrics=['accuracy']) 
+model.summary()
+# fitting the model
+his = model.fit( 
+  train_data,
+    validation_data=valid_data, 
+  epochs=15 
+)
+# evaluate the model
+y_test = valid_data.classes
+y_pred = model.predict(valid_data)
+y_pred = np.argmax(y_pred,axis=1)
+
+model.save("Sight-SageProject.tf")
+loaded_model = tf.keras.models.load_model('Sight-SageProject.tf')
+
+from keras.preprocessing import image
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))  # Adjust target_size based on your model requirements
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+# Provide the path to your image
+image_path = r'_21_4533748.jpg'  # Replace with the actual path to your image
+
+# Preprocess the image
+processed_image = preprocess_image(image_path)
+predictions = loaded_model.predict(processed_image)
+disease_labels = ['glaucoma', 'cataract', 'normal', 'diabetic_retinopathy']
+df_predict = pd.DataFrame(predictions)
+max_values_per_row = df_predict.idxmax(axis=1)
+m = [disease_labels[i] for i in max_values_per_row]
+print(m)
